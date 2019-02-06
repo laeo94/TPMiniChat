5b28abc59d732eddff07bdce8caf50d0a1c0bcbf
laeo94 <amandaong@hotmail.fr>
2019-02-06 14:29:20 +0100

    projet

---
+++ Java Projet/src/block/ColorGraphic.java
@@ -0,0 +1,10 @@
+package block;
+
+import java.awt.Graphics;
+
+import game.Pos;
+
+public abstract class ColorGraphic {
+	abstract public void paintCompartment(Graphics g , int caseSize, Pos view);
+}
+ 
